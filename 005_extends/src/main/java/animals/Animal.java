1e5c6a79fc21fe4153c090ebc65a3cb6a0594b46
mizhidan <dj9824@126.com>
2020-02-13 17:47:44 +0800

    [danjie] feat: animal class

---
+++ 005_extends/src/main/java/animals/Animal.java
@@ -0,0 +1,23 @@
+package animals;
+
+public class Animal {
+    int age;
+    int weight;
+
+    public Animal() {
+
+    }
+
+    public Animal(int inputAge,int inputWeight) {
+        age = inputAge;
+        weight = inputWeight;
+    }
+
+    public void eat() {
+        
+    }
+
+    public void sleep() {
+
+    }
+}
